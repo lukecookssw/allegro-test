9d90ae1dce3fc4f9119ca50f601fb1cbbad9a1d2
Luke Cook <lukecook@ssw.com.au>
2025-11-15 09:07:20 +0800

    being adding spatial grid

--- lib/collision/spatial_grid.c
+++ lib/collision/spatial_grid.c
@@ -1,3 +1,24 @@
-
 #include "spatial_grid.h"
+#include <stdlib.h>
 
+SpatialGrid* grid_create(int world_w, int world_h, int cell_w, int cell_h)
+{
+    SpatialGrid* grid   = malloc(sizeof(SpatialGrid));
+
+    grid->cell_height   = cell_h;
+    grid->cell_width    = cell_w;
+
+    grid->world_height  = world_h;
+    grid->world_width   = world_w;
+
+    return grid;
+}
+
+void grid_clear(SpatialGrid* grid);
+void grid_insert(SpatialGrid* grid, Circle* circle);
+void grid_get_nearby_circles(SpatialGrid* grid, Circle* circle, CircleList* out);
+
+void grid_destroy(SpatialGrid* grid)
+{
+    free(grid);
+}
--- lib/collision/spatial_grid.h
+++ lib/collision/spatial_grid.h
@@ -1,7 +1,37 @@
 #ifndef SPATIAL_GRID_H
 #define SPATIAL_GRID_H
 
+#include "../circle/circle.h"
 
+struct CircleNode;
 
+typedef struct CircleNode
+{
+    Circle* circle;
+    struct CircleNode* next;
+} CircleNode;
+
+typedef struct
+{
+    CircleNode* head;
+    int count;
+} CircleList;
+
+typedef struct
+{
+    int rows;
+    int columns;
+    int cell_width;
+    int cell_height;
+    int world_width;
+    int world_height;
+    CircleList** cells;
+} SpatialGrid;
+
+SpatialGrid* grid_create(int world_w, int world_h, int cell_w, int cell_h);
+void grid_clear(SpatialGrid* grid);
+void grid_insert(SpatialGrid* grid, Circle* circle);
+void grid_get_nearby_circles(SpatialGrid* grid, Circle* circle, CircleList* out);
+void grid_destroy(SpatialGrid* grid);
 
 #endif
